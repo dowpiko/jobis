e744f7248b13617e5bd4c5d814977912f975ad57
yangMinjae <ymj137955@naver.com>
2025-06-30 12:42:08 +0900

    index.js 수정

---
+++ frontend/jobis/src/data/categories.js
@@ -0,0 +1,50 @@
+const categories = 
+[
+	{
+		"category": "IT / 소프트웨어 개발",
+		"subCategories": [
+			{
+				"name": "웹 개발자",
+				"description": "웹사이트 및 웹앱 개발",
+				"skills": ["HTML", "CSS", "JavaScript", "React", "Vue", "Spring", "Django"]
+			},
+			{
+				"name": "프론트엔드 개발자",
+				"description": "UI 개발 중심",
+				"skills": ["React", "Vue.js", "TypeScript", "Webpack"]
+			},
+			{
+				"name": "백엔드 개발자",
+				"description": "서버, DB, API 개발",
+				"skills": ["Java", "Spring", "Node.js", "Python", "Django", "MySQL", "MongoDB"]
+			},
+			{
+				"name": "앱 개발자",
+				"description": "모바일 앱 개발",
+				"skills": ["Kotlin", "Swift", "Flutter", "React Native"]
+			},
+			{
+				"name": "게임 개발자",
+				"description": "게임 엔진 기반 콘텐츠 개발",
+				"skills": ["Unity", "Unreal Engine", "C#", "C++"]
+			},
+			{
+				"name": "데이터 엔지니어",
+				"description": "데이터 수집/처리",
+				"skills": ["Python", "SQL", "Hadoop", "Spark", "Airflow"]
+			},
+			{
+				"name": "AI 개발자",
+				"description": "머신러닝/딥러닝 개발",
+				"skills": ["Python", "TensorFlow", "PyTorch", "Scikit-learn"]
+			},
+			{
+				"name": "보안 전문가",
+				"description": "시스템 및 정보 보안",
+				"skills": ["Linux", "Firewall", "암호화", "OWASP", "WAS", "SIEM"]
+			}
+		]
+	}
+];
+
+export default categories;
