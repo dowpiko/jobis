5153eb22b57a73bdec553885e206d03adccf0c5b
배상민 <sfad7@naver.com>
2025-07-16 16:05:08 +0900

    app.js 구조 변경

--- frontend/jobis/src/App.js
+++ frontend/jobis/src/App.js
@@ -29,6 +29,9 @@
   import VideoChat from './components/chat/VideoChat';
   import CompanyChatLayout from './components/company/CompanyChatLayout';
   import UserChatLayout from './components/UserChatLayout';
+  import UserSidebarLayout from './components/layout/UserSidebarLayout';
+  import ProfileSidebarLayout from './components/layout/ProfileSidebarLayout';
+import CompanySidebarLayout from './components/layout/CompanySidebarLayout';
 
 
 
@@ -38,219 +41,58 @@
       <BrowserRouter>
         <Routes>
           {/* 기본 경로 - 회원가입 선택 */}
-          <Route
-            path="/signUp"
-            element={
-                <SignUp/>
-            }
-          />
+          <Route path="/signUp" element={<SignUp/>}/>
 
           {/* 로그인 화면 */}
-          <Route
-            path="/"
-            element={
-                <Login />
-            }
-          />
+          <Route path="/" element={<Login />}/>
 
           {/* 네이버 로그인 콜백 경로 */}
-          <Route 
-            path="/naver/callback" 
-            element={
-              <NaverCallback />
-            } 
-          />
+          <Route path="/naver/callback" element={<NaverCallback />}/>
 
           {/* 카카오 로그인 콜백 경로 */}
-          <Route 
-            path="/kakao/callback"
-            element={
-              <KakaoCallback/>
-            } 
-          /> 
+          <Route path="/kakao/callback" element={<KakaoCallback/>}/>
 
           {/* 구글 로그인 콜백 경로 */}
-          <Route 
-            path="/google/callback" 
-            element={
-              <GoogleCallback />
-            } 
-          /> 
+          <Route path="/google/callback" element={<GoogleCallback />}/> 
 
           {/* 개인 회원가입 */}
-          <Route
-            path="/signUpUser"
-            element={
-                <SignUpUser />
-            }
-          />
-          
+          <Route path="/signUpUser" element={<SignUpUser />}/>
 
           {/* 기업 회원가입 */}
-          <Route path="/signUpCmp" element={
-                <SignUpCmp />
-            }
-          />
+          <Route path="/signUpCmp" element={<SignUpCmp />}/>
 
           {/*백그라운드만 띄우기 */ }
-            <Route path="/UserSidebar" element={
-                <UserSidebar />
-            }
-          />
+          <Route path="/UserSidebar" element={<UserSidebar />}/>
+
           {/* 프로필 선택(AI or 화상채팅)  */}
-          <Route
-            path="/profile"
-            element={              
-              <Profile/>
-            }
-          />
-          
-          {/* AI 모의 면접  */}
-          <Route
-            path="/aiInterview"
-            element={
-              
-                  <UserSidebar>
-                    <AiInterview />
-                </UserSidebar>
-            }
-          />
-          {/* 프로필 생성  */}
-          <Route
-            path="/createProfile"
-            element={
-              
-                  <ProfileSidebar>
-                    <CreateProfile/>
-                </ProfileSidebar>
-            }
-          />
-          {/* 프로필 생성  */}
-          <Route
-            path="/createProfileForm"
-            element={
-              
-                  <ProfileSidebar>
-                    <CreateProfileForm/>
-                </ProfileSidebar>
-            }
-          />
-          {/* 화상 모의 면접  */}
-          <Route
-            path="/scheduleManager"
-            element={
-              
-                  <ProfileSidebar>
-                    <ScheduleManager/>
-                </ProfileSidebar>
-            }
-          />
+          <Route path="/profile" element={<Profile/>}/>
+
           {/* 화상 채팅 */}
           <Route path="/video" element={<VideoChat />} />
-        {/* AI 인터뷰 생성 페이지  */}
-          <Route
-            path="/createAiInterview"
-            element={
-              <UserSidebar>
-                <CreateAiInterview/>
-              </UserSidebar>
-            }
-          />
-        {/* AI 인터뷰 진행 페이지  */}
-          <Route
-            path="/AiChat"
-            element={
-              <UserSidebar>
-                <AiChat/>
-              </UserSidebar>
-            }
-          />
-          {/* 데이터 시각화 페이지  */}
-          <Route
-            path="/graphPage"
-            element={
-              <UserSidebar>
-                <GraphPage/>
-              </UserSidebar>
-            }
-          />
-          {/* 디스코드 페이지  */}
-          <Route
-            path="/discordPage"
-            element={
-              
-                  <ProfileSidebar>
-                    <DiscordPage/>
-                </ProfileSidebar>
-            }
-          />
-          {/* 기업 공고 정보  */}
-          <Route
-            path="/companyInfo"
-            element={
-              <UserSidebar>
-                <CompanyInfo/>
-              </UserSidebar>
-            }
-          />
-          {/* 공고 지원 페이지  */}
-          <Route
-            path="/applyNotice"
-            element={
-              <UserSidebar>
-                <ApplyNotice/>
-              </UserSidebar>
-            }
-          />
-          {/* 스크립 & 지원  */}
-          <Route
-            path="/scrapPage"
-            element={
-              <UserSidebar>
-                <ScrapPage/>
-              </UserSidebar>
-            }
-          />
+
+          <Route element={<UserSidebarLayout />}>
+            <Route path="/userChatLayout" element={<UserChatLayout />} />           {/* 유저 채팅 */}
+            <Route path="/scrapPage" element={<ScrapPage />} />                     {/* 스크랩 & 지원 */}
+            <Route path="/applyNotice" element={<ApplyNotice />} />                 {/* 공고 지원 페이지 */}
+            <Route path="/companyInfo" element={<CompanyInfo />} />                 {/* 기업 공고 정보 */}
+            <Route path="/graphPage" element={<GraphPage />} />                     {/* 데이터 시각화 페이지 */}
+            <Route path="/AiChat" element={<AiChat />} />                           {/* AI 인터뷰 진행 페이지 */}
+            <Route path="/createAiInterview" element={<CreateAiInterview />} />     {/* AI 인터뷰 생성 페이지 */}
+            <Route path="/aiInterview" element={<AiInterview />} />                 {/* AI 모의 면접 */}
+          </Route>
+
+          <Route element={<ProfileSidebarLayout />}>
+            <Route path="/createProfile" element={<CreateProfile />} />             {/* 프로필 생성 */}
+            <Route path="/createProfileForm" element={<CreateProfileForm />} />     {/* 프로필 폼 작성 */}
+            <Route path="/scheduleManager" element={<ScheduleManager />} />         {/* 화상 모의 면접 */}
+            <Route path="/discordPage" element={<DiscordPage />} />                 {/* 디스코드 페이지 */}
+          </Route>
           
-          {/*  기업 메인(공고 진행중)   */}
-          <Route
-            path="/companyMain"
-            element={
-              <CompanySidebar>
-                <CompanyMain/>
-              </CompanySidebar>
-            }
-          />
-          {/* 공고 등록 */}
-          <Route
-            path="/noticeProgress"
-            element={
-              <CompanySidebar>
-                <NoticeProgress/>
-              </CompanySidebar>
-            }
-          />
-
-          {/* 기업 채팅 */}
-          <Route
-            path="/companyChatLayout"
-            element={
-              <CompanySidebar>
-                <CompanyChatLayout/>
-              </CompanySidebar>
-            }
-          />
-
-          {/* 유저 채팅 */}
-          <Route
-            path="/userChatLayout"
-            element={
-              <UserSidebar>
-                <UserChatLayout/>
-              </UserSidebar>
-            }
-          />
-
+          <Route element={<CompanySidebarLayout />}>
+            <Route path="/companyMain" element={<CompanyMain />} />                 {/* 기업 메인(공고 진행중) */}
+            <Route path="/noticeProgress" element={<NoticeProgress />} />           {/* 공고 등록 */}
+            <Route path="/companyChatLayout" element={<CompanyChatLayout />} />     {/* 기업 채팅 */}
+          </Route>
         </Routes>
       </BrowserRouter>
     );
