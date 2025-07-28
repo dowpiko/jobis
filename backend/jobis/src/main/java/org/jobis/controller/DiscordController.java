3839273c233112a131ad2b2c1a5a454c036e3c8b
yangMinjae <ymj137955@naver.com>
2025-07-28 15:17:17 +0900

    .

--- backend/jobis/src/main/java/org/jobis/controller/DiscordController.java
+++ backend/jobis/src/main/java/org/jobis/controller/DiscordController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:3000"}, allowCredentials = "true")
+@CrossOrigin(origins = {"http://localhost:3000","http://192.168.0.101:3000"}, allowCredentials = "true")
 @RequestMapping("/discord")
 public class DiscordController {
 	@Autowired
